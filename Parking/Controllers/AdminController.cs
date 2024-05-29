6e34d74ee38fd9b957a850a13304f700a5dca624
Antoni Prochownik <toni.prochownik@gmail.com>
2024-05-29 11:35:27 +0200

    add deleting users

--- Parking/Controllers/AdminController.cs
+++ Parking/Controllers/AdminController.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.CodeAnalysis.FlowAnalysis.DataFlow.CopyAnalysis;
 using Microsoft.EntityFrameworkCore;
 using Parking.Models;
 
@@ -18,6 +19,27 @@ namespace Parking.Controllers
         {
             var users = await _userManager.Users.ToListAsync();
             return View(users);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> DeleteUser(string id)
+        {
+            var user = await _userManager.FindByIdAsync(id);
+            if (user == null)
+            {
+                return NotFound();
+            }
+            var result = await _userManager.DeleteAsync(user);
+            if (result.Succeeded)
+            {
+                TempData["SuccessMessage"] = "User has been successfully deleted.";
+            }
+            else
+            {
+                TempData["ErrorMessage"] = "Error deleting user.";
+            }
+            
+            return RedirectToAction("ListUsers");
         }
     }
 }
