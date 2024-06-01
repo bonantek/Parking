cb5156648285b67efb8c4e3f329ed9de975c9abb
mateus-po <mateusz.porebski99@gmail.com>
2024-06-01 19:05:04 +0200

    Update parking slot availability after creating reservation

--- Parking/Controllers/ReservationController.cs
+++ Parking/Controllers/ReservationController.cs
@@ -12,19 +12,16 @@ namespace Parking.Controllers
     public class ReservationController : Controller
     {
         private readonly AppDbContext _context;
-        private readonly IParkingService _parkingService;
         private readonly ICarService _carService;
         private readonly IReservationService _reservationService;
         private readonly UserManager<ApplicationUser> _userManager;
         
         public ReservationController(AppDbContext context,
-            IParkingService parkingService,
             ICarService carService,
             IReservationService reservationService,
             UserManager<ApplicationUser> userManager)
         {
             _context = context;
-            _parkingService = parkingService;
             _carService = carService;
             _reservationService = reservationService;
             _userManager = userManager;
@@ -33,12 +30,14 @@ namespace Parking.Controllers
         [Authorize]
         public async Task<IActionResult> Create(System.Guid id)
         {
-            var parking = await _parkingService.GetByIdAsync(id);
+            var parking = await _context.Parkings
+                .Include(p => p.ParkingSlots)
+                .ThenInclude(ps => ps.Reservations)
+                .SingleAsync(p => p.Id == id);
             if (parking == null)
             {
                 return Redirect("/Home/404");
             }
-            await _context.Entry(parking).Collection(p => p.ParkingSlots).LoadAsync();
             ViewData["Parking"] = parking;
 
             var user = await _userManager.GetUserAsync(HttpContext.User);
@@ -55,13 +54,15 @@ namespace Parking.Controllers
         public async Task<IActionResult> CreatePost(System.Guid id,
             [Bind("StartDate", "EndDate", "CarId", "ParkingSlotId")] Reservation reservation)
         {
-            var parking = await _parkingService.GetByIdAsync(id);
+            var parking = await _context.Parkings
+                .Include(p => p.ParkingSlots)
+                .ThenInclude(ps => ps.Reservations)
+                .SingleAsync(p => p.Id == id);
             if (parking == null)
             {
                 return Redirect("/Home/404");
             }
             
-            await _context.Entry(parking).Collection(p => p.ParkingSlots).LoadAsync();
             ViewData["Parking"] = parking;
             var user = await _userManager.GetUserAsync(HttpContext.User);
             if (user == null)
@@ -74,14 +75,14 @@ namespace Parking.Controllers
             reservation.UserId = user.Id;
             try
             {
-                await _reservationService.CreateReservation(reservation);
+                reservation = await _reservationService.CreateReservation(reservation);
                 TempData["SuccessMessage"] = "Successfully created a reservation.";
             }
             catch (Exception ex)
             {
                 TempData["ErrorMessage"] = $"Error while creating a reservation: {ex.Message}";
             }
-            return View();
+            return View(reservation);
         }
 
         [Authorize]
